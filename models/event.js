a83f97244014fc30fb9b9981760d1165ecf551e8
gohai <gottfried.haider@gmail.com>
2017-02-25 15:50:05 +0100

    Beautify schema

--- models/event.js
+++ models/event.js
@@ -2,21 +2,21 @@
 var mongoose = require("mongoose");
 
 var eventSchema = mongoose.Schema({
-	address: { type: String},
-	address2: { type: String},
-	amComposer: { type: Boolean},
-	city: { type: String},
-	country: { type: String},
-	date: { type: String},
-	location: { type: String},
-	longDescription: { type: String},
-	piece: { type: String},
-	price: { type: String},
-	state: { type: String},
-	time: { type: String},
-	title: { type: String},
-	venue: { type: String},
-	zip: { type: String}
+	address: { type: String },
+	address2: { type: String },
+	amComposer: { type: Boolean },
+	city: { type: String },
+	country: { type: String },
+	date: { type: String },
+	location: { type: String },
+	longDescription: { type: String },
+	piece: { type: String },
+	price: { type: String },
+	state: { type: String },
+	time: { type: String },
+	title: { type: String },
+	venue: { type: String },
+	zip: { type: String }
 });
 
 var Event = mongoose.model("events", eventSchema);
